d38ba47925caa77e3204cd8d8ea97fdba5724349
ARSAL27 <arsalking882@gmail.com>
2026-04-17 11:26:22 +0500

    v3.3.18 - Final Chart Stabilization & Clean Console

--- KiryanaBook/src/pages/ReportsDetail.tsx
+++ KiryanaBook/src/pages/ReportsDetail.tsx
@@ -133,7 +133,7 @@ export const ReportsDetail: React.FC = () => {
                         </div>
 
                         <div className="h-60 w-full mb-4">
-                            <ResponsiveContainer width="100%" height="100%">
+                            <ResponsiveContainer width="100%" height="100%" minWidth={0} minHeight={0} debounce={100}>
                                 <AreaChart data={chartData} margin={{ top: 10, right: 0, left: -20, bottom: 0 }}>
                                     <defs>
                                         <linearGradient id="colorSales" x1="0" y1="0" x2="0" y2="1">
